430b991d7391cb09295a757fd61e131ecab32c2f
heather sterman <heather.sterman531@gmail.com>
2018-10-22 17:32:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(lunch)
+  lunch = {"salad" => 5, "iced_tea" => 1.50, "cookie" => 1.50}
+key_for_min_value(lunch)
 end
